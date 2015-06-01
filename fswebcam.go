d32bd81bc86ecc041ebc9c283f8fb868876ced75
root <matthew.mcneely@gmail.com>
2015-06-01 15:00:28 -0400

    First commit of go wrapper and test

---
+++ fswebcam.go
@@ -0,0 +1,62 @@
+package fswebcam
+
+//fswebcam -d v4l2:/dev/video1 -S 2 -p MJPEG -v -r 1280x720 -fps 30 foo.jpg
+
+/*
+
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <locale.h>
+#include <gd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "fswebcam.h"
+#include "log.h"
+#include "src.h"
+#include "dec.h"
+#include "effects.h"
+#include "parse.h"
+
+#cgo LDFLAGS: -lgd
+#cgo CFLAGS: -g -O2 -DHAVE_CONFIG_H
+
+extern gdImage* getCapture(int argc, char *argv[]);
+
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+func GetCaptureJPEGBytes(args []string, quality int) []byte {
+	argc := C.int(len(args))
+	argv := make([]*C.char, argc)
+	for i, arg := range args {
+		argv[i] = C.CString(arg)
+	}
+
+	img := C.getCapture(argc, &argv[0])
+	bytes := ImageToJPEGBuffer(img, quality)
+
+	C.gdImageDestroy(img)
+	for _, arg := range argv {
+		C.free(unsafe.Pointer(arg))
+	}
+	return bytes
+}
+
+func ImageToJPEGBuffer(p *C.gdImage, quality int) []byte {
+	var imgSize int
+	pimgSize := (*C.int)(unsafe.Pointer(&imgSize))
+
+	buf := C.gdImageJpegPtr(p, pimgSize, C.int(quality))
+	defer C.gdFree(buf)
+
+	return C.GoBytes(buf, *pimgSize)
+}
---
+++ fswebcam_test.go
@@ -0,0 +1,25 @@
+package fswebcam
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestFrameGrab(t *testing.T) {
+
+	args := strings.Split("-d v4l2:/dev/video1 -S 2 -p MJPEG -q -r 960x720 -F 2 -fps 30", " ")
+	bytes := GetCaptureJPEGBytes(args, 100)
+	if len(bytes) < 1000 {
+		t.Errorf("Len of captured bytes should be greater %d\n", len(bytes))
+	}
+	ioutil.WriteFile("output.jpg", bytes, 0666)
+}
+
+func BenchmarkFrameGrab(b *testing.B) {
+
+	args := strings.Split("-d v4l2:/dev/video1 -S 1 -p MJPEG -q -r 960x720 -F 1", " ")
+	for i := 0; i < b.N; i++ {
+		GetCaptureJPEGBytes(args, 100)
+	}
+}
